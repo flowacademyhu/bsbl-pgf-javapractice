6871391d31f1df7b0f647310b3ac791b01ad42d6
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-04-08 22:02:55 +0200

    Database storage implemented

--- HTMLArticleParser/src/main/java/ArticleParser.java
+++ HTMLArticleParser/src/main/java/ArticleParser.java
@@ -6,16 +6,19 @@ import org.jsoup.select.Elements;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ArticleParser {
 
+
     public static void main(String[] args) {
         ArticleParser parser = new ArticleParser();
-        Document searchResults = parser.parseUrl("https://index.hu/24ora/?word=1&pepe=1&tol=1999-01-01&ig=2018-04-07&s=soros+gy%C3%B6rgy");
+        Document searchResults = parser.parseUrl("https://index.hu/24ora/?word=1&pepe=1&tol=1999-01-01&ig=2018-04-08&s=microsoft");
         Elements articles = parser.getArticles(searchResults);
         ArrayList<String> articleUrls = parser.getUrlList(articles);
-        parser.parseArticles(articleUrls);
+        //parser.parseArticles(articleUrls);
+        parser.articlesToDB(articleUrls);
     }
 
     public Document parseUrl(String url) {
@@ -61,7 +64,30 @@ public class ArticleParser {
         for(int i = 0; i < urls.size(); i++) {
             Document doc = parseUrl(urls.get(i));
             writeArticles("/home/papp/articles/article" + (i + 1) + ".txt", doc.title() + "\n");
-            writeArticles("/home/papp/articles/article" + (i + 1) + ".txt", doc.getElementsByClass("cikk-torzs").text());
+            writeArticles("/home/papp/articles/article" + (i + 1) + ".txt", doc.getElementsByTag("p").text());
         }
+    }
+
+    public void articlesToDB(ArrayList<String> urls) {
+        java.sql.Connection connection;
+        String db = "jdbc:mysql://localhost:3306/articleDB";
+        String user = "root";
+        String password = "toor";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(db, user, password);
+            Statement statement = connection.createStatement();
+            for(int i = 0; i < urls.size(); i++) {
+                Document doc = parseUrl(urls.get(i));
+                statement.executeUpdate("insert into articles (title, content) values ('" + doc.title() + "', '" + doc.body().getElementsByClass("cikk-torzs").text().replace("'", "") + "');");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
