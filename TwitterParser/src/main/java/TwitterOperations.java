9723b7d2abb7a8bb0d4260d665d52729471caae7
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-04-25 13:04:37 +0200

    commited some projects

---
+++ TwitterParser/src/main/java/TwitterOperations.java
@@ -0,0 +1,54 @@
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public final class TwitterOperations {
+    public static void main(String[] args) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("rVcADZmS5sfVxyTq0pSbZD4Hv")
+                .setOAuthConsumerSecret("mcUafNuLWyBCn1OGqqHmppICJZ1VtgCudmK6X2tcqOzLlRPYvX")
+                .setOAuthAccessToken("989077134747602948-SIvLiEYDlzp2zz0wYdtxybRGtAewtgE")
+                .setOAuthAccessTokenSecret("fZDKXEdRUKvAX8cIktjmxqASCbOmpLaCrYd0EdzPA8cuM");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+
+        TwitterOperations to = new TwitterOperations();
+        /*
+        try {
+            to.createTweet("This my first tweet through TwitterAPI.", tf);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        */
+        to.getTimeline("realDonaldTrump", tf, 6, 100);
+    }
+
+    public void createTweet(String tweet, TwitterFactory tf) throws TwitterException {
+        Twitter twitter = tf.getInstance();
+        Status status = twitter.updateStatus(tweet);
+    }
+
+    public void getTimeline(String userName, TwitterFactory tf, int pageNumber, int count) {
+        Twitter twitter = tf.getInstance();
+        try {
+            List<Status> statuses;
+            Paging page = new Paging (pageNumber, count);
+            User user = twitter.showUser(userName);
+            statuses = twitter.getUserTimeline(user.getId(), page);
+            int tweetNumber = 1;
+            for (Status status : statuses) {
+                System.out.println(tweetNumber + " @" + status.getUser().getScreenName() + " - " + status.getText());
+                tweetNumber++;
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+}
